f5dbef8690e7f06c46fc0854d137f77460bf61a2
streamich <streamich@gmail.com>
2024-07-06 21:25:06 +0200

    feat: 🎸 add ability to human-readable print debug trace

--- src/print.ts
+++ src/print.ts
@@ -1,6 +1,14 @@
 import {printTree} from 'tree-dump/lib/printTree';
-import {isListNode, isProductionNode, isProductionShorthandNode, isRefNode, isTerminalNode, isTerminalShorthandNode, isUnionNode} from './util';
-import type {Grammar, GrammarNode} from './types';
+import {
+  isListNode,
+  isProductionNode,
+  isProductionShorthandNode,
+  isRefNode,
+  isTerminalNode,
+  isTerminalShorthandNode,
+  isUnionNode,
+} from './util';
+import type {Grammar, GrammarNode, ParseTraceNode, RootTraceNode} from './types';
 
 export class GrammarPrinter {
   public static readonly print = (grammar: Grammar, tab?: string): string => {
@@ -20,7 +28,12 @@ export class GrammarPrinter {
   public printNode(node: GrammarNode, type?: string, tab?: string): string {
     if (type) this.visited.add(type);
     if (isTerminalNode(node)) {
-      const pattern = typeof node.t === 'string' ? JSON.stringify(node.t) : Array.isArray(node.t) ? '(' + node.t.map(c => JSON.stringify(c)).join(' | ') + ')' + (node.repeat ?? '') : node.t;
+      const pattern =
+        typeof node.t === 'string'
+          ? JSON.stringify(node.t)
+          : Array.isArray(node.t)
+            ? '(' + node.t.map((c) => JSON.stringify(c)).join(' | ') + ')' + (node.repeat ?? ' ')
+            : node.t;
       return `${node.type ?? type ?? 'Text'} (terminal): ${pattern}`;
     } else if (isTerminalShorthandNode(node)) {
       return this.printNode({t: node}, type, tab);
@@ -32,16 +45,62 @@ export class GrammarPrinter {
         else return this.printNode(this.grammar.cst[reference], reference, tab);
       }
       this.visited.add(reference);
-      return `${type ?? 'Reference'} (reference)` + (visited ? ' →' : printTree(tab, [(tab) => this.printNode(this.grammar.cst[reference], reference, tab)]));
+      return (
+        `${type ?? 'Reference'} (reference)` +
+        (visited ? ' →' : printTree(tab, [(tab) => this.printNode(this.grammar.cst[reference], reference, tab)]))
+      );
     } else if (isProductionNode(node)) {
-      return `${node.type ?? type ?? 'Production'} (production)` + printTree(tab, node.p.map((n) => (tab) => this.printNode(n, undefined, tab)));
+      return (
+        `${node.type ?? type ?? 'Production'} (production)` +
+        printTree(
+          tab,
+          node.p.map((n) => (tab) => this.printNode(n, undefined, tab)),
+        )
+      );
     } else if (isProductionShorthandNode(node)) {
       return this.printNode({p: node}, type, tab);
     } else if (isUnionNode(node)) {
-      return `${node.type ?? type ?? 'Union'} (union)` + printTree(tab, node.u.map((n) => (tab) => this.printNode(n, undefined, tab)));
+      return (
+        `${node.type ?? type ?? 'Union'} (union)` +
+        printTree(
+          tab,
+          node.u.map((n) => (tab) => this.printNode(n, undefined, tab)),
+        )
+      );
     } else if (isListNode(node)) {
-      return `${node.type ?? type ?? 'List'} (list)` + printTree(tab, [(tab) => this.printNode(node.l, undefined, tab)]);
+      return (
+        `${node.type ?? type ?? 'List'} (list)` + printTree(tab, [(tab) => this.printNode(node.l, undefined, tab)])
+      );
     }
     return 'unknown';
-  };
+  }
 }
+
+export const printTraceNode = (trace: RootTraceNode | ParseTraceNode, tab?: string, text?: string): string => {
+  const pattern = (trace as ParseTraceNode).ptr;
+  const type = pattern?.type ?? ((trace as ParseTraceNode).ptr ? 'Anonymous' : 'Root');
+  const match = (trace as ParseTraceNode).match;
+  let matchFormatted = '';
+  if (match) {
+    matchFormatted = ' ' + match.pos + ':' + match.end;
+    if (text) {
+      const end = match.pos + Math.min(32, match.end - match.pos);
+      const slice = text.slice(match.pos, end) + (end !== match.end ? '...' : '');
+      const sliceFormatted =
+        slice.indexOf('"') >= 0 && slice.indexOf("'") === -1
+          ? "'" + JSON.stringify(slice).slice(1, -1).replaceAll('\\"', '"') + "'"
+          : JSON.stringify(slice);
+      matchFormatted += ' → ' + sliceFormatted;
+    }
+  }
+  return (
+    `${type}` +
+    matchFormatted +
+    (trace.children && trace.children.length
+      ? printTree(
+          tab,
+          trace.children.map((n) => (tab) => printTraceNode(n, tab, text)),
+        )
+      : '')
+  );
+};
